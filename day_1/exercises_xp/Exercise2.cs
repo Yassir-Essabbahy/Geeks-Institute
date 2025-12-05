45b3581bd039e167bd93890dad95397ab4c0ddda
Yassir-Essabbahy <moroccoyassir@gmail.com>
2025-12-05 12:37:40 +0100

    first init

---
+++ day_1/exercises_xp/Exercise2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Exercise2
+{
+    public static void Run()
+    {
+        string name = "Yassir";
+        int age = 20;
+        Console.WriteLine($"My name is {name} and I am {age} years old.");
+    }
+}
