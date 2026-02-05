c7e8ee1a8f52b6b9d183e63eba313eb3e17c15c3
Yassir-Essabbahy <moroccoyassir@gmail.com>
2026-02-05 14:38:56 +0100

    updatee

--- week_1/day_1/exercises_xp_gold/fizzbuzz.cs
+++ week_1/day_1/exercises_xp_gold/fizzbuzz.cs
@@ -6,17 +6,15 @@ class Exercise1
     {
         Console.WriteLine("Enter A Number Between 0 and 100 : ");
         int num = int.Parse(Console.ReadLine());
-        if (num % 5 == 0 && num % 3 == 0)
-        {
-            Console.WriteLine($"{num} is  a Multiplication of 3 and 5");
-        }
-        else if (num % 5 == 0)
-        {
-            Console.WriteLine($"{num} is  a Multiplication of 5");
-        }
-        else if (num % 3 == 0)
-        {
-            Console.WriteLine($"{num} is  a Multiplication of 3");
+                    if(number %3 == 0 && number %5 == 0){
+                Console.WriteLine("FizzBuzz");
+            }
+            else if(number %3 == 0 && number%5 != 0){
+                Console.WriteLine("Fizz");
+            }
+            else if(number %3 != 0 && number%5 == 0){
+                Console.WriteLine("Buzz");
+            
         }
     }
 }
---
+++ week_7/day_5/Mini_Project/README.md
@@ -0,0 +1,19 @@
+## 🎮 To Download Alpha Build
+ 
+🔹 Platform: Windows (PC)
+
+
+🔹  https://drive.google.com/drive/folders/178e0j6wUxZQNN9ex0Q9G9bU-sxrW6MgX?usp=sharing
+
+
+## 📸 Screenshot
+
+![unity](s1.png)
+
+![GamePlay](s2.png)
+
+![unity2](s3.png)
+
+![unity3](s4.png)
+
+![unity4](s5.png)
